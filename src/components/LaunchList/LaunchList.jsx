2500215b91135c5e67179691118e62657b953e00
madhu-p-gith <sai.vikasvarma.p@gmail.com>
2020-12-24 17:00:37 +0530

    feching data

--- src/components/LaunchList/LaunchList.jsx
+++ src/components/LaunchList/LaunchList.jsx
@@ -1,34 +1,43 @@
 import React, { Component } from "react";
 import Launch from "../Launch/Launch";
 import "./styles.css";
+import axios from "axios"; //node modules
 class LaunchList extends Component {
+  state = {
+    launches: [],
+  };
+  componentDidMount = () => {
+    this.getLaunches();
+  };
+  getLaunches = () => {
+    axios
+      .get("https://api.spacexdata.com/v3/launches")
+      .then((response) => {
+        this.setState({
+          launches: response.data,
+        });
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  launchList = () => {
+    const launchListComponents = this.state.launches.map((launch1) => {
+      return (
+        <Launch
+          banner={launch1.links.flicker_images[0]}
+          title={launch1.mission_name}
+          launchDate={launch1.launch_date_local}
+          description={launch1.details}
+          launch={launch1.mission_name}
+        />
+      );
+    });
+    return launchListComponents;
+  };
   render() {
-    return (
-      <div className="launch-list">
-        <Launch
-          banner={
-            "https://timesofindia.indiatimes.com/thumb/msid-66945579,imgsize-751944,width-400,resizemode-4/66945579.jpg"
-          }
-          title={"Folcon sat"}
-          launchDate={"2002-03-12"}
-          description={
-            "Hi thkjdskjfkdsjhfkl dhgkhfgflkj ghdfkjhgdfj fdlkjhvgkjdfhgfk jhfrjkdh hfd "
-          }
-          launch="focono"
-        />
-        <Launch
-          banner={
-            "https://timesofindia.indiatimes.com/thumb/msid-66945579,imgsize-751944,width-400,resizemode-4/66945579.jpg"
-          }
-          title={"Folcon sat"}
-          launchDate={"2002-03-12"}
-          description={
-            "Hi thkjdskjfkdsjhfkl dhgkhfgflkj ghdfkjhgdfj fdlkjhvgkjdfhgfk jhfrjkdh hfd "
-          }
-          launch="focono"
-        />
-      </div>
-    );
+    return <div className="launch-list">{this.launchList()}</div>;
   }
 }
 
